0a9bbb6bd2d0a76b1fe3e530ea323dbc663a1a22
kevin cao <1103746556@qq.com>
2021-12-15 11:45:03 +0800

    feat(eslintrc.js): eslint babel 解析方式 es6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
   root: true,
-  plugins: [ 'react' ],
+  plugins: ['react'],
   globals: {
-    PKG_VERSION: true,
+    PKG_VERSION: true
   },
   settings: {
     'import/resolver': {
       node: null,
       webpack: {
-        config: 'build/webpack-configs/base.js',
-      },
-    },
+        config: 'build/webpack-configs/base.js'
+      }
+    }
   },
   rules: {
-    "space-before-function-paren": ["error", "always"],
+    'space-before-function-paren': ['error', 'always'],
     'import/no-named-as-default': 0,
     'unicorn/consistent-function-scoping': 0,
     'vue/attributes-order': 0,
@@ -22,14 +22,22 @@ module.exports = {
     'no-console': 0,
     'no-warning-comments': 0,
     'no-undefined': 0,
-    'prefer-destructuring': 0,
+    'prefer-destructuring': 0
   },
-  overrides: [ {
-    files: [ 'src/**' ],
-    rules: {
-      'unicorn/no-for-loop': 0,
-      'unicorn/prefer-includes': 0,
-      'unicorn/prefer-node-append': 0,
-    },
-  } ],
+  overrides: [
+    {
+      files: ['src/**'],
+      rules: {
+        'unicorn/no-for-loop': 0,
+        'unicorn/prefer-includes': 0,
+        'unicorn/prefer-node-append': 0
+      }
+    }
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    parserOptions: {
+      ecmaVersion: 'es6'
+    }
+  }
 }
